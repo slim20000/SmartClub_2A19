49f93af080b4d7120829b8cc769e6e32bf3e5e98
slim20000 <76734226+slim20000@users.noreply.github.com>
2021-04-07 21:13:18 +0200

    Add files via upload

---
+++ PRODUIT/produit.h
@@ -0,0 +1,40 @@
+#ifndef PRODUIT_H
+#define PRODUIT_H
+#include <QString>
+#include <QDate>
+#include <QComboBox>
+#include <QSqlQueryModel>
+class produit
+{
+public:
+    produit();
+    produit(int,QDate,QString,QString,QString,QString);
+    int getid();
+
+    QString getnom();
+    QString getprixachat();
+    QString getprix_vente();
+    QString getcategorie();
+    QDate getage();
+    void setid(int);
+    void setnom(QString);
+    void setprixachat(QString);
+   void setprix_vente(QString);
+    void setcategorie(QString);
+    void setage(QDate);
+    bool ajouter();
+    bool update(int);
+    bool removeRows();
+    QSqlQueryModel * afficher();
+    bool supprimer(int);
+    QSqlQueryModel * search1(QString);
+    QSqlQueryModel * order1();
+    QSqlQueryModel * order3();
+
+private:
+    int id;
+    QDate age;
+    QString nom,prixachat,categorie,prix_vente;
+};
+
+#endif // PRODUIT_H
